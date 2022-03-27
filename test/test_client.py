3d3d0e51273f13449cda3c2981af7223c957ff3a
NestorAlban <albquenes7294@gmail.com>
2022-03-26 20:57:04 -0500

    unit_testing1

--- test/test_client.py
+++ test/test_client.py
@@ -1,5 +1,8 @@
+import json
 from fastapi.testclient import TestClient
+import pytest
 from app.main import app
+
 
 client = TestClient(app)
 
@@ -9,3 +12,12 @@ def test_valid_id():
     response = client.get("/user/3")
     assert response.status_code == 200
     assert response.json() == [{"idper": 3, "name": "fa", "mail": "fa@example.com", "active": True}]
+
+
+def test_create_userok():
+    user = {"idper": 16, "name": "bo", "mail": "bo@example.com"}
+    response = client.post("/registeruser_status", json.dumps(user))
+    assert response.status_code == 201, response.text
+    data = response.json()
+    assert data["mail"] == user["mail"]
+    assert data["name"] == user["name"]
